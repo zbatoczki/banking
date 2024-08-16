c9534dc26b3c998fa674d3f2ae0a5b90d8c418fe
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2024-08-15 20:19:13 -0700

    implement header and total current balance sections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstname:'Zoltan'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox 
+              type="greeting"
+              title="Welcome, "
+              user={loggedIn?.firstname || 'Guest'}
+              subtext="Access and manage your account and transactions."
+            />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks = {1}
+            totalCurrentBalance = {420.69}
+          />
+
+          </header>
+      </div>
+    </section>
   )
 }
 
