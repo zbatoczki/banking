456be8b80e526728216fe55e6b42ad08d0b70332
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2024-08-17 15:54:30 -0700

    Implement responsive navigation section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,30 @@
+import MobileNav from "@/components/ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName: 'Zoltan', lastName: 'Batoczki'};
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src='/icons/logo.svg' width={30} height={30} alt='Horizon menu icon' />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
+
+        
     </main>
   );
 }
